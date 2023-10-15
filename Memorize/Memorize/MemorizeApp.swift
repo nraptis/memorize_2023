7d3eaca1d77f789136182fc925c3b55063d02c71
Nick Raptis <nraptis@gmail.com>
2023-10-14 20:41:08 -0400

    started the project

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Nick Raptis on 10/14/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
