758662a4b1713822630ed87142e171f5b83b8d74
Nicky Taylor <nickytaylor@Nickys-MacBook-Pro.local>
2023-10-15 00:10:05 -0400

    end of lexture 1

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -9,13 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
         }
+        .foregroundColor(.orange)
         .padding()
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(lineWidth: 4.0, antialiased: false)
+                Text("🌎")
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.purple)
+            } else {
+                RoundedRectangle(cornerRadius: 12)
+            }
+        }
     }
 }
 
