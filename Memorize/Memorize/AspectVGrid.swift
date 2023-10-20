d551ad581c27d42e2069df88b8356c0b32f061f2
Nick Raptis <nraptis@gmail.com>
2023-10-19 21:33:06 -0400

    end of lecture 6

---
+++ Memorize/Memorize/AspectVGrid.swift
@@ -0,0 +1,18 @@
+//
+//  AspectVGrid.swift
+//  Memorize
+//
+//  Created by Nicky Taylor on 10/19/23.
+//
+
+import SwiftUI
+
+struct AspectVGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AspectVGrid()
+}
